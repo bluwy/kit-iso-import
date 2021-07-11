c4322317f07e21421f902e0e2611bd5f4e6d50fd
bluwy <bjornlu.dev@gmail.com>
2021-07-12 00:29:16 +0800

    Use vite-plugin-iso-import

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import MagicString from 'magic-string';
+import { isoImport } from 'vite-plugin-iso-import';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,39 +6,9 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
-			plugins: [iso()]
+			plugins: [isoImport()]
 		}
 	}
 };
 
 export default config;
-
-function iso() {
-	const clientImportRe = /import\s*(?:[\w*{}\n\r\t, ]+\s*from\s*)?(?:"[^"]+\?client"|'[^']+\?client')/gm;
-	const serverImportRe = /import\s*(?:[\w*{}\n\r\t, ]+\s*from\s*)?(?:"[^"]+\?server"|'[^']+\?server')/gm;
-
-	return {
-		name: 'vite-plugin-iso',
-		enforce: 'post',
-		resolveId(importee, importer, customOptions, ssr) {
-			if (ssr ? importee.endsWith('?server') : importee.endsWith('?client')) {
-				return importee.slice(0, -7);
-			}
-		},
-		transform(code, id, ssr) {
-			let s,
-				m,
-				re = ssr ? clientImportRe : serverImportRe;
-			while ((m = re.exec(code)) != null) {
-				if (!s) s = new MagicString(code);
-				s.overwrite(m.index, m.index + m[0].length, '');
-			}
-			if (s) {
-				return {
-					code: s.toString(),
-					map: s.generateMap()
-				};
-			}
-		}
-	};
-}
